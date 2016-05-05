5fe695fb09c412af3daeb4eb07e956387982c37b
Matt <mpayne77@gmail.com>
2016-05-05 23:12:34 +0000

    Disabled SQLAlchemy modification tracking

--- config.py
+++ config.py
@@ -15,4 +15,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'blog-dev.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
-
+SQLALCHEMY_TRACK_MODIFICATIONS = False
