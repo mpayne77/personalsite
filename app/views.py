8427c86af111469677bd630694a01880402abb8b
Matt <mpayne77@gmail.com>
2016-05-05 19:12:23 +0000

    Added posts to index

--- app/views.py
+++ app/views.py
@@ -1,12 +1,13 @@
 from flask import render_template, flash, redirect
-from app import app
+from app import app, models
 from .forms import LoginForm
 from passlib.hash import sha256_crypt
 
 @app.route('/')
 @app.route('/index/')
 def index():
-    return render_template('index.html')
+    posts = models.BlogPost.query.all()
+    return render_template('index.html', posts=posts)
     
 
 @app.route('/login/', methods=['GET', 'POST'])
